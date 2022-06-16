2289849e2506ade56b43415df7ce5972fbd492c5
Lewis Russell <lewis6991@gmail.com>
2022-06-16 10:01:59 +0100

    fix: don't error if nvim-web-devicons isn't installed

--- lua/brodir/handlers/icons.lua
+++ lua/brodir/handlers/icons.lua
@@ -2,11 +2,14 @@ local api = vim.api
 
 local ns = api.nvim_create_namespace('brodir.handlers.icons')
 
-local get_icon = require('nvim-web-devicons').get_icon
+local ok, devicons = pcall(require, 'nvim-web-devicons')
 
 return function(buf, _, lines)
+  if not ok then
+    return
+  end
   for i, l in ipairs(lines) do
-    local icon, icon_hl = get_icon(l, nil, {default=true})
+    local icon, icon_hl = devicons.get_icon(l, nil, {default=true})
     api.nvim_buf_set_extmark(buf, ns, i-1, 0, {
       sign_text = icon,
       sign_hl_group = icon_hl
